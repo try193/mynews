9e784fbc4b4d5d33104ed8e4fe171224b8a4d9ca
”谷藤諒哉” <uyan1931052@gmail.com>
2020-10-10 01:26:11 +0000

    mynews commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,13 +1,13 @@
-@extends('layouts.profile')
+@extends('layouts.admin')
 
 
-@section('title', 'MYプロフィール')
+@section('title', 'ニュースの新規作成')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>MYプロフィール</h2>
+                <h2>ニュース新規作成</h2>
             </div>
         </div>
     </div>
