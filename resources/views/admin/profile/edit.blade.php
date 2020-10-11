7b88b79a22e88ca9eb4e22c385cfdfd9bc6c9667
”谷藤諒哉” <uyan1931052@gmail.com>
2020-10-11 08:15:44 +0000

    mynews commit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Myprofileedit</title>
-    </head>
-    
-    <body>
-        <h1>Myプロファイル編集画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+
+@section('title', 'プロフィール編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+            </div>
+        </div>
+    </div>
+@endsection
