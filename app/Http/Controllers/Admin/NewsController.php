2630eac8287c0366d1a1edc7f9c3c29b7b63d1ce
”谷藤諒哉” <uyan1931052@gmail.com>
2020-10-20 02:17:49 +0000

    mynews commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -11,7 +11,8 @@ class NewsController extends Controller
     public function add(){
         return view("admin.news.create");
     }
-    
+    // Requestクラスは、ブラウザを通してユーザーから送られる情報をすべて含んでいるオブジェクトを取得することができる。
+    // Requestを$requestに代入して使用している
     public function create(Request $request){
         // Varidationを行う
         $this->validate($request,News::$rules);
@@ -40,4 +41,54 @@ class NewsController extends Controller
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
+    
+    public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      if (empty($news)) {
+        abort(404);    
+      }
+      return view('admin.news.edit', ['news_form' => $news]);
+  }
+
+
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, News::$rules);
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      
+      unset($news_form['_token']);
+      unset($news_form['remove']);
+      if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
+
+      return redirect('admin/news');
+  }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin','middleware'=>'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create','Admin\NewsController@create');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create','Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit','Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit','Admin\NewsController@update')->middleware('auth');
+    
 });
-
 
 Route::get('XXX','Admin\AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
 });
 Auth::routes();
 
