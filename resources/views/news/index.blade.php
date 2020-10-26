e102930cb149e610379d7e40ff1ea8f502df634f
”谷藤諒哉” <uyan1931052@gmail.com>
2020-10-26 03:48:12 +0000

    Heroku課題

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <hr color="#c0c0c0">
+        <p>追加開発しました。</p>
         @if (!is_null($headline))
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
